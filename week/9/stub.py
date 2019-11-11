abc3a81d79513b4aeffdeaacaf87842be4a78c03
Moses Kamoga <kamogamoses12@gmail.com>
2019-11-11 10:00:50 -0500

    writeup

--- week/9/stub.py
+++ week/9/stub.py
@@ -2,6 +2,8 @@
 
 import sys
 import struct
+from datetime import datetime
+import base64
 
 
 # You can use this method to exit on failure conditions.
@@ -36,7 +38,99 @@ print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
 
+#printing the timestamp
+timestamp, = struct.unpack("<l", data[8:12])
+print("TIMESTAMP: %d (%s)" % (timestamp, datetime.fromtimestamp(timestamp)))
+
+#printing the author name
+author = struct.unpack("<cccccccc", data[12:20])
+print(author)
+
+#Number of sections count
+section_count, = struct.unpack("<L", data[20:24])
+print("SECTION COUNT: %d" % section_count)
+
+#data length
+print("DATA LENGTH : %d" % len(data))
+
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
 
+print(" ")
 print("-------  BODY  -------")
+
+
+
+#methods to help me with the printing of characters of the messages
+def print_char(entry, end, encoding):
+    char, = entry
+    char = char.decode(encoding)
+    sys.stdout.write(char)
+
+def print_int(entry, end):
+    num, = entry
+    sys.stdout.write(num)
+
+def print_me(entry, end):
+    print(entry)
+
+
+# type: (size,format_string,end)
+stype_dict = {
+    1: (1, "<c", print_char, {'end': "", 'encoding' : 'ascii'}),
+    2: (1, "<c", print_char, {'end': "", 'encoding' : 'utf-8'}),
+    3: (4, "<L", print_int, {'end': ""}),
+    4: (8, "<q", print_me , {'end': ""}),
+    5: (8, "<d", print_int, {'end' : ""}),
+    6: (16, "<dd", print_me , {'end': ""}),
+    7: (4, "<L", print_int, {'end': ""})
+}
+
+offset = 24
+
+section_count = 0
+
+while (offset < len(data)):
+
+    section_count += 1
+
+    type_s, len_s = struct.unpack_from("<LL", data, offset)
+    
+    print(" ")
+    print("SECTION TYPE: %d" % type_s)
+    print("SECTION LENGTH: %d" % len_s)
+
+    offset += 8
+    start_offset = offset
+
+    if (type_s in stype_dict):
+
+        if (len_s == 80):
+            decoded_string = base64.b64decode(data[offset:offset+len_s])
+            print("The decoded String is -----> %s" %decoded_string)
+
+        elif (len_s == 44):
+             print("Reversed String, Another flag found:---> %s" %data[offset:offset+len_s][::-1])
+
+        (size,format_string,out_fun,args) = stype_dict[type_s]
+
+        for index in range(0, int(len_s / size)):
+
+            entry = struct.unpack_from(format_string, data, offset)
+            offset += size
+            out_fun(entry, **args)
+        
+
+
+        print(" ")
+        
+    if (offset != start_offset + len_s):
+        print("ENTRY SKIPPED")
+        offset = start_offset + len_s
+
+    
+
+print(" ")
+print("-------  BODY  -------")
+
+print("NUMBER OF SECTIONS %d" % section_count)
--- week/9/writeup/README.md
+++ week/9/writeup/README.md
@@ -1,15 +1,84 @@
 # Writeup 9 - Forensics II
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: *Moses Kamoga*
+Section: *0101*
 
 I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: *Moses Kamoga*
 
 
 ## Assignment details
 
 ### Part 1 (45 Pts)
+1. 142.93.136.81
+   Using wireshark and analysizing the communication between networks.
+   I realized that this IP sent the SYN-ACK to the attacking IP. 
+   Meaning that the IP was vulnerable. and to complete the communication.
+   We observe that the attacking IP sent an ACK to the attacked IP above.
+
+2. nmap
+   From wireshark, we observe that the attacking IP is sending SYNs to multiple ports at the attacked 
+   IP address. 
+   Thus, the tool that does this is a nmap which is used to send various SYNs to ports
+   until a vulnerable port is found.
+
+3. 159.203.113.181
+   This is the IP address observed to be sending out various SYNs. 
+   Hence meaning, it is the attacking IP address. 
+
+4. 21
+   We can observer that this port sent back a SYN + ACK.
+   Which means a channel of communication was established when 
+   the attacking IP address sent an ACK.
+
+5. findme,
+   it is a jpeg.
+   The file is a picture.
+
+6. greetz.fpff
+
+7. You can run a Nmap on your system and find out which ports are open.
+   When you find these ports, you can determine if they need to be patched or not
+   depending on what inofrmation one can access by attacking those ports.
+
+   Also using a firewall, you can turn a hackers couple of minutes work into a 
+   7 hour work by redirecting their port scans. 
 
 ### Part 2 (55 Pts)
+
+i.    TIMESTAMP: 1553660105 (2019-03-27 00:15:05)
+
+ii.   Fl1nch
+
+iii.  SECTION TYPE: 1
+      SECTION LENGTH: 24
+      Hey you, keep looking :) 
+ 
+      SECTION TYPE: 6
+      SECTION LENGTH: 16
+      (52.336035, 4.880673)
+ 
+ 
+      SECTION TYPE: 8
+      SECTION LENGTH: 202776
+      ENTRY SKIPPED
+ 
+      SECTION TYPE: 1
+      SECTION LENGTH: 44
+      Reversed String, Another flag found:---> CMSC389R-{h0pefully_y0u_didnt_grep_CMSC389R}
+      }R983CSMC_perg_tndid_u0y_yllufep0h{-R983CSMC 
+ 
+      SECTION TYPE: 1
+      SECTION LENGTH: 80
+      The decoded String is -----> CMSC389R-{hey_h3y_y0U_you_I_dont_like_your_base64_encoding}
+      Q01TQzM4OVIte2hleV9oM3lfeTBVX3lvdV9JX2RvbnRfbGlrZV95b3VyX2Jhc2U2NF9lbmNvZGluZ30= 
+ 
+
+
+iv.   CMSC389R-{h0pefully_y0u_didnt_grep_CMSC389R}
+
+      CMSC389R-{hey_h3y_y0U_you_I_dont_like_your_base64_encoding}
+
+
+
